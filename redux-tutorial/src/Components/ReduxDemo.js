323d56c72d8b2b0c719c45fc3530feb676fb31ed
Katmore2727 <katmoreyash2121@gmail.com>
2024-08-30 13:19:11 +0530

    updated

--- redux-tutorial/src/Components/ReduxDemo.js
+++ redux-tutorial/src/Components/ReduxDemo.js
@@ -1,31 +1,65 @@
-// import React from 'react';
+// import { createStore } from 'redux';
+
+// export default function ReduxDemo(){
+
+// const reducer =(state = 0, action)=>{
+//     if(action.type === 'INCREMENT'){
+//         return state + 1;
+//     }else if(action.type === 'DECREMENT'){
+//         return state - 1;
+//     }
+//     return state;
+// }
+
+// const store = createStore(reducer);
+
+// store.subscribe(()=>{
+//     console.log('currentState', store.getState());
+// });
+
+// store.dispatch({
+//     type:'INCREMENT'
+// });
+
+// store.dispatch({
+//     type:'DECREMENT'
+// });
+
+// }
+
+
+import React, { useEffect, useState } from 'react';
 import { createStore } from 'redux';
 
-export default function ReduxDemo(){
-
-const reducer =(state = 0, action)=>{
-    if(action.type === 'INCREMENT'){
+const reducer = (state = 0, action) => {
+    if (action.type === 'INCREMENT') {
         return state + 1;
-    }else if(action.type === 'DECREMENT'){
+    } else if (action.type === 'DECREMENT') {
         return state - 1;
     }
     return state;
+};
+
+const store = createStore(reducer); // Move store creation outside
+
+export default function ReduxDemo() {
+    const [currentState, setCurrentState] = useState(store.getState());
+
+    useEffect(() => {
+        const unsubscribe = store.subscribe(() => {
+            setCurrentState(store.getState());
+        });
+
+        // Dispatch actions
+        store.dispatch({ type: 'INCREMENT' });
+        store.dispatch({ type: 'DECREMENT' });
+
+        return () => unsubscribe();
+    }, []); // No dependencies needed since store is outside
+
+    return (
+        <div>
+            <h1>Redux State: {currentState}</h1>
+        </div>
+    );
 }
-
-const store = createStore(reducer);
-
-store.subscribe(()=>{
-    console.log('currentState', store.getState());
-});
-
-store.dispatch({
-    type:'INCREMENT'
-});
-
-store.dispatch({
-    type:'DECREMENT'
-});
-
-}
-
-// export default store;
